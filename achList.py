606ae703bb8a7f6fe44ceaaa6bf093cafa00d4e6
stuffyerface <77872467+stuffyerface@users.noreply.github.com>
2021-03-08 19:38:01 -0500

    Create achList.py

---
+++ achList.py
@@ -0,0 +1,9 @@
+c_general = ["general_first_join","general_use_portal","general_first_game","general_first_chat","general_youtuber","general_first_friend","general_first_party","general_friends_25","general_creeperbook","general_vip","general_vip_plus","general_a_long_journey_begins","general_achievement_npc","general_treasure_hunt","general_hot_potato"]
+t_general = [("general_coins",250000),("general_wins",2500),("general_quest_master",250),("general_challenger",500)]
+
+c_housing = ["housing_join_guild","housing_join_friend","housing_join_staff","housing_join_youtube","housing_become_resident","housing_give_cookie","housing_new_look","housing_recieve_cookie","housing_join_random","housing_make_resident","housing_grand_opening"]
+t_housing = []
+
+
+achs = [(c_general,t_general,"Max General"),(c_housing,t_housing,"Max Housing")]
+c_legacy = ["general_gifting"]
