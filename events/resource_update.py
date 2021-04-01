8eeb988d413a0c9c429a6345bf19c750beec21c8
stuffyerface <77872467+stuffyerface@users.noreply.github.com>
2021-04-01 10:27:56 -0400

    Create resource_update.py

---
+++ events/resource_update.py
@@ -0,0 +1,33 @@
+from events.base_event      import BaseEvent
+import requests
+import json
+from datetime               import datetime, date
+from math                   import floor
+import discord
+import settings
+
+
+# Your friendly example event
+# You can name this class as you like, but make sure to set BaseEvent
+# as the parent class
+
+class ResourceUpdate(BaseEvent):
+
+    def __init__(self):
+        interval_minutes = 20  # Set the interval for this event
+        super().__init__(interval_minutes)
+
+    # Override the run() method
+    # It will be called once every {interval_minutes} minutes
+    
+    async def run(self, client):
+        apres = requests.get("https://api.hypixel.net/resources/achievements")
+        achres = json.loads(apres.text)
+        if achres["success"] == True:
+          lu = achres["lastUpdated"]
+          if lu != settings.RESOURCE_LAST_UPDATED:
+            if(settings.RESOURCE_LAST_UPDATED != 0):
+              rt = datetime.utcfromtimestamp(lu/1000).strftime('%m/%d/%Y %H:%M')
+              channel = client.get_channel(818611323755036702)
+              await channel.send(content = f"Resources Updated: {rt}")
+            settings.RESOURCE_LAST_UPDATED = lu
