a749c75a24f46e45b8b3a094011f7b4e3df32c3b
stuffyerface <77872467+stuffyerface@users.noreply.github.com>
2021-05-10 22:55:40 -0400

    Create random.py

---
+++ commands/random.py
@@ -0,0 +1,41 @@
+from commands.base_command  import BaseCommand
+from utils                  import *
+from random                 import randint
+
+
+# Your friendly example event
+# Keep in mind that the command name will be derived from the class name
+# but in lowercase
+
+# So, a command class named Random will generate a 'random' command
+
+class Random(BaseCommand):
+
+    def __init__(self):
+        # A quick description for the help message
+        description = "Gives info on the current tournament"
+        # A list of parameters that the command will take as input
+        # Parameters will be separated by spaces and fed to the 'params' 
+        # argument in the handle() method
+        # If no params are expected, leave this list empty or set it to None
+        params = []
+        super().__init__(description, params)
+
+    # Override the handle() method
+    # It will be called every time the command is received
+    async def handle(self, params, message, client):
+        # 'params' is a list that contains the parameters that the command 
+        # expects to receive, t is guaranteed to have AT LEAST as many
+        # parameters as specified in __init__
+        # 'message' is the discord.py Message object for the command to handle
+        # 'client' is the bot Client object
+        
+        if(checkperm(message.author)):
+          progress = randomAp()
+          progress = apFormat(progress)
+
+
+
+          await message.channel.send(content = "", embed=progress)
+        else:
+          await message.channel.send(content = "", embed = embeds.emPremium)
