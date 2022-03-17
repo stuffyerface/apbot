9e8119707e65c8353643d2c5c8e03d75fbe21e97
stuffyerface <77872467+stuffyerface@users.noreply.github.com>
2022-03-17 03:51:24 -0400

    Update staff.py

--- staff.py
+++ staff.py
@@ -5,4 +5,35 @@ STAFF_LIST = [
   ("Stuffy", "2cfc8db571ed4eb3aacd53b8abff5ee2", "ADMIN")
 ]
 
-ONLINE_LIST = [["2cfc8db571ed4eb3aacd53b8abff5ee2", "Stuffy", "{}", 0],["78800595c0e9445595ac4a93fa2a3a4c", "Pensul", "{}", 0],["16ddab826eb74fff81d1e1566a3c6d19", "Bloozing", "{}", 0],["fb178c95d152459eb8dcefd127894e38", "Jayavarmen", "{}", 0],["27987a70f78a498faccca76ae2f55d8d", "Judg3", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "Citria", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "TorWolf", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "LadyBleu", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "JacobRuby", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "Revengeee", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "flameboy101", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "Fr0z3n", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "ConnorLinfoot", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "Plancke", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "Donpireso", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "inventivetalent", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "_PolynaLove_", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "Minikloon", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "Dctr", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "xHascox", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "sfarnham", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "TheMGRF", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "Bembo", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "Cheesy", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "OrangeMarshall", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "Sylent_", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "0b111", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "Pickguard", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "Rozsa", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "Liffeh", "{}", 0],["73f64a5ec87a48feab8c059acf4ba848", "skyerzz", "{}", 0]]
+ONLINE_LIST = [["2cfc8db571ed4eb3aacd53b8abff5ee2", "Stuffy", "{}", 0],
+               ["78800595c0e9445595ac4a93fa2a3a4c", "Pensul", "{}", 0, False],
+               ["16ddab826eb74fff81d1e1566a3c6d19", "Bloozing", "{}", 0, False],
+               ["fb178c95d152459eb8dcefd127894e38", "Jayavarmen", "{}", 0, False],
+               ["27987a70f78a498faccca76ae2f55d8d", "Judg3", "{}", 0, False],
+               ["73f64a5ec87a48feab8c059acf4ba848", "Citria", "{}", 0, False],
+               ["156776fa1d1a47a2b4c3eb514916548f", "TorWolf", "{}", 0, False],
+               ["ef35e10b189e4193a7c9c4813e76af05", "LadyBleu", "{}", 0, False],
+               ["2225eb44c7b648af8f0c0f04d905fb3c", "JacobRuby", "{}", 0, False],
+               ["3329eda9a1234d08af339c9ec398d747", "Revengeee", "{}", 0, False],
+               ["4d5027c8619b46d090de394fc6ab1f6c", "flameboy101", "{}", 0, False],
+               ["9ca76de8b66f4711a433e86d3f236d49", "Fr0z3n", "{}", 0, False],
+               ["ad8fefaa8351454bb739a4eaa872173f", "ConnorLinfoot", "{}", 0, False],
+               ["f025c1c7f55a4ea0b8d93f47d17dfe0f", "Plancke", "{}", 0, False],
+               ["f4aa8fe99f3e4c9ea474c2e80fa95589", "Donpireso", "{}", 0, False],
+               ["bcd2033c63ec4bf88aca680b22461340", "inventivetalent", "{}", 0, False],
+               ["1f5b04ce01d743b8a385019be12612ff", "_PolynaLove_", "{}", 0, False],
+               ["20934ef9488c465180a78f861586b4cf", "Minikloon", "{}", 0, False],
+               ["0593eb6757a241d2afde4517e00a35b5", "Dctr", "{}", 0, False],
+               ["0e410e560df24000b585d51ab67b1099", "xHascox", "{}", 0, False],
+               ["812ebfe22b154b5c98b5d6c8a49cb23f", "sfarnham", "{}", 0, False],
+               ["c3f82c10348e4ab5b2a0d9a4f6c67602", "TheMGRF", "{}", 0, False],
+               ["0fdadef7a92946b29982b69ba27b6557", "Bembo", "{}", 0, False],
+               ["321fe5bb2db747d398b22fd99675d3ae", "Cheesey", "{}", 0, False],
+               ["9354f81e9f7947288fafcaee9e4c2c93", "OrangeMarshall", "{}", 0, False],
+               ["51e8915dbe3a4d5894693a16f3c60c73", "Sylent_", "{}", 0, False],
+               ["ff91b20b5d97411282a3f2ef46a50b64", "0b111", "{}", 0, False],
+               ["641d083dbd0e4ac693d3135b43d4407a", "Pickguard", "{}", 0, False],
+               ["48725972cb2143c1902db0a54ba3615a", "Rozsa", "{}", 0, False],
+               ["bfcbc3e7c2be4a3197ecb39c68455869", "Liffeh", "{}", 0, False],
+               ["1460c760829b4a3c8db7ebf19d724113", "skyerzz", "{}", 0, False]
+            ]
