acdf8127b09d5be9b02d210aa26875ebd112df85
Stuffy <stuffinessmc@gmail.com>
2024-09-02 22:18:50 -0400

    message intents

--- Stuffy.py
+++ Stuffy.py
@@ -24,7 +24,7 @@ sched = AsyncIOScheduler()
 def main():
     # Initialize the client
     print("Starting up...")
-    client = discord.Client(intents=discord.Intents.all())
+    client = discord.Client(intents=discord.Intents.messages())
 
     # Define event handlers for the client
     # on_ready may be called multiple times in the event of a reconnect,
